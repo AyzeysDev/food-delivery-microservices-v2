45849e81d06408d40bf4a563f0e27a6172addd64
Aravind Kumar <ara3194@gmail.com>
2021-04-29 00:54:52 +0530

    Configuration changes

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,13 +4,13 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
 
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      headers: req.headers
+      baseURL: 'http://www.food-microservices-aravind.xyz',
+      headers: req.headers,
     });
   } else {
 
     return axios.create({
-      baseUrl: '/'
+      baseUrl: '/',
     });
   }
 };
